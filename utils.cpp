b025596c2e5768ebdc2df479c825b79212e38abe
julius-richter <julius.marius.richter@googlemail.com>
2019-12-21 22:53:33 +0100

    clean repo

--- utils.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <string>
-#include "utils.h"
-
-void print(std::string String)
-{
-    std::cout << String << "\n";
-}
--- utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-	#define UTILS_H
-
-	void print(std::string String);
-
-#endif
-
